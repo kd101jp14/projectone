93c9b81f245fc862f5af0dcddcb9b772414418f5
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-03-11 00:27:56 -0500

    Updated IDs for input and button. Mood now appears on the UI.

---
+++ assets/javascript/app.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+
+    var userInput = $("#userInput").val().trim();
+
+    var queryURL = "https://apis.paralleldots.com/v3/emotion?" + "text=" + userInput + "&api_key=XHEmlBGDQ6f880Y8TmdJFizVECdu2hUo7sGZWbzbfhQ"
+
+    $("#submitButton").on("click", function () {
+
+        $("#moodOutput").empty();
+        $("#playlistOutput").empty();
+
+        $.ajax({
+            url: queryURL,
+            method: "POST"
+        }).then(function (response) {
+            console.log(response);
+            console.log(response.emotion.emotion);
+
+            if (response.emotion.emotion === "Angry") {
+                $("#moodOutput").append("<p>Angry</p>");
+                $("#playlistOutput").append();
+            }
+            if (response.emotion.emotion === "Bored") {
+                $("#moodOutput").append("<p>Bored</p>");
+                $("#playlistOutput").append();
+            }
+            if (response.emotion.emotion === "Excited") {
+                $("#moodOutput").append("<p>Excited</p>");
+                $("#playlistOutput").append();
+            }
+            if (response.emotion.emotion === "Fear") {
+                $("#moodOutput").append("<p>Fear</p>");
+                $("#playlistOutput").append();
+            }
+            if (response.emotion.emotion === "Happy") {
+                $("#moodOutput").append("<p>Happy</p>");
+                $("#playlistOutput").append();
+            }
+            if (response.emotion.emotion === "Sad") {
+                $("#moodOutput").append("<p>Sad</p>");
+                $("#playlistOutput").append();
+            };
+            $("#userInput").empty();
+
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            console.error(errorThrown);
+        });
+    });
+});
