b9cca19c1ac01c0d4541c41d30583324cb61bfee
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-03-11 13:05:44 -0500

    Removed event.preventDefault(). Moved around some variables to see if the ajax internal server error issue, as well as the input sticking issue can be resolved.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,56 +1,57 @@
 $(document).ready(function () {
 
-    var userInput = $("#userInput").val().trim();
+    var userInput = $("#userInput").val();
 
     var queryURL = "https://apis.paralleldots.com/v3/emotion?" + "text=" + userInput + "&api_key=XHEmlBGDQ6f880Y8TmdJFizVECdu2hUo7sGZWbzbfhQ"
-
-    $("#submitButton").on("click", function () {
+    
+    $("#submitButton").on("click", function (event) {
 
         $("#moodOutput").empty();
         $("#playlistOutput").empty();
+        $("playlistHolder").empty();
 
         $.ajax({
             url: queryURL,
             method: "POST"
         }).then(function (response) {
-            console.log(response);
-            console.log(response.emotion.emotion);
-
             var emotionResponse = response.emotion.emotion;
+            
+            console.log(response);
+            console.log(emotionResponse);
 
-            $("#journalEntry").prepend("<hr>");
-            $("#journalEntry").prepend(userInput);
             $("#moodOutput").append("<p>" + emotionResponse + "</p>");
             $("#playlistOutput").append("<div>").addClass("playlistHolder");
+            $("#journalEntry").prepend("<hr>");
+            $("#journalEntry").prepend(userInput);
            
-            if (response.emotion.emotion === "Angry") {
+            if (emotionResponse === "Angry") {
                 // Add appropriate playlist.
-                $("playlistHolder").append()
+                $("playlistHolder").append();
             }
-            if (response.emotion.emotion === "Bored") {
+            if (emotionResponse === "Bored") {
                // Add appropriate playlist.
-                $("playlistHolder").append()
+                $("playlistHolder").append();
             }
-            if (response.emotion.emotion === "Excited") {
+            if (emotionResponse === "Excited") {
                // Add appropriate playlist.
-                $("playlistHolder").append()
+                $("playlistHolder").append();
             }
-            if (response.emotion.emotion === "Fear") {
+            if (emotionResponse === "Fear") {
                // Add appropriate playlist.
-                $("playlistHolder").append()
+                $("playlistHolder").append();
             }
-            if (response.emotion.emotion === "Happy") {
+            if (emotionResponse === "Happy") {
                // Add appropriate playlist.
-                $("playlistHolder").append()
+                $("playlistHolder").append();
             }
-            if (response.emotion.emotion === "Sad") {
+            if (emotionResponse === "Sad") {
                // Add appropriate playlist.
-                $("playlistHolder").append()
+                $("playlistHolder").append();
             };
             $("#userInput").val("");
 
         }).fail(function (jqXHR, textStatus, errorThrown) {
-            console.error(errorThrown);
+            console.error(jqXHR, textStatus, errorThrown);
         });
     });
 });
