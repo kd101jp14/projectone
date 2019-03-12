14c15276dc711bcf9f54aea2f0b424b6357df927
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-03-12 12:07:28 -0500

    Copied over fixes from 'paralleldots-fixes' branch.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,57 +1,28 @@
-$(document).ready(function () {
+$(document).ready(function() {
+  $("#submitButton").on("click", function(event) {
+    event.preventDefault();
 
-    var userInput = $("#userInput").val();
+    var userInput = $("#userInput")
+      .val()
+      .trim();
 
-    var queryURL = "https://apis.paralleldots.com/v3/emotion?" + "text=" + userInput + "&api_key=XHEmlBGDQ6f880Y8TmdJFizVECdu2hUo7sGZWbzbfhQ"
-    
-    $("#submitButton").on("click", function (event) {
+    var queryURL =
+      "https://apis.paralleldots.com/v3/emotion?text=" +
+      userInput +
+      "&api_key=XHEmlBGDQ6f880Y8TmdJFizVECdu2hUo7sGZWbzbfhQ";
 
-        $("#moodOutput").empty();
-        $("#playlistOutput").empty();
-        $(".playlistHolder").empty();
+    $.ajax({
+      url: queryURL,
+      method: "POST"
+    })
+      .then(function(response) {
+        var emotion = response.emotion.emotion;
+        $("#moodOutput").text(emotion);
 
-        $.ajax({
-            url: queryURL,
-            method: "POST"
-        }).then(function (response) {
-            var emotionResponse = response.emotion.emotion;
-            
-            console.log(response);
-            console.log(emotionResponse);
-
-            $("#moodOutput").append("<p>" + emotionResponse + "</p>");
-            $("#playlistOutput").append("<div>").addClass("playlistHolder");
-            $("#journalEntry").prepend("<hr>");
-            $("#journalEntry").prepend(userInput);
-           
-            if (emotionResponse === "Angry") {
-                /// Embed appropriate playlist between plus signs.
-                $(".playlistHolder").append("<div> </div>");
-            }
-            if (emotionResponse === "Bored") {
-               // Embed appropriate playlist between plus signs.
-                $(".playlistHolder").append("<div>" + + "</div>");
-            }
-            if (emotionResponse === "Excited") {
-               /// Embed appropriate playlist between plus signs.
-                $(".playlistHolder").append("<div> </div>");
-            }
-            if (emotionResponse === "Fear") {
-               /// Embed appropriate playlist between plus signs.
-                $(".playlistHolder").append("<div> </div>");
-            }
-            if (emotionResponse === "Happy") {
-               /// Embed appropriate playlist between plus signs.
-                $(".playlistHolder").append("<div> </div>");
-            }
-            if (emotionResponse === "Sad") {
-               /// Embed appropriate playlist between plus signs.
-                $(".playlistHolder").append("<div> </div>");
-            };
-            $("#userInput").val("");
-
-        }).fail(function (jqXHR, textStatus, errorThrown) {
-            console.error(jqXHR, textStatus, errorThrown);
-        });
-    });
+        // Put Spotify Conditionals here
+      })
+      .fail(function(jqXHR, textStatus, errorThrown) {
+        console.error(errorThrown);
+      });
+  });
 });
