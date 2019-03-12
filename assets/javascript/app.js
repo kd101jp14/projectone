55b45cde9b0db0f975e3ceaed1dea793e6b63567
Luke Duran <ltduran.lsp@gmail.com>
2019-03-11 22:51:30 -0500

    Got the parallel dots stuff to work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,26 +15,19 @@ $.ajax({
 
 
 
-
-
-$(document).ready(function () {
-
+$("#submitButton").on("click",function(event){
+    event.preventDefault();
     var userInput = $("#userInput").val().trim();
+    var queryURL = "https://apis.paralleldots.com/v3/emotion?text=" + userInput + "&api_key=XHEmlBGDQ6f880Y8TmdJFizVECdu2hUo7sGZWbzbfhQ"
 
-    var queryURL = "https://apis.paralleldots.com/v3/emotion?" + "text=" + userInput + "&api_key=XHEmlBGDQ6f880Y8TmdJFizVECdu2hUo7sGZWbzbfhQ"
+    $.ajax({
+        url: queryURL,
+        method: "POST"
+    }).then(function(response){
+        var emotion = response.emotion.emotion;
+        $("#moodOutput").text(emotion);
 
-    $("#submitButton").on("click", function () {
-        console.log("clicked");
-
-        $.ajax({
-            url: queryURL,
-            method: "POST"
-        }).then(function (response) {
-            console.log(response);
-            console.log("Hi");
-        }).fail(function ( jqXHR, textStatus, errorThrown) {
-            console.error(errorThrown);
-        });
-    });
-
+        // Put Spotify Conditionals here
+    })
 });
+
